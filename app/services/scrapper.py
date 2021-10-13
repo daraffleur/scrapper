059e5248ff818401b6a24f6c3cd4741779863150
Dana <romaniuk.dana@gmail.com>
2021-10-13 17:00:30 +0300

    arrange scroll profile page function

--- app/services/scrapper.py
+++ app/services/scrapper.py
@@ -1,5 +1,35 @@
+import time
+
+
 class ScrapperService:
     def __init__(self, driver, holdup: int, linkedin_base_url: str):
         self.driver = driver
         self.holdup = holdup
         self.linkedin_base_url = linkedin_base_url
+
+    def scroll_profile_page(self):
+        """Get scroll height"""
+        last_scroll_height = self.driver.execute_script(
+            "return document.body.scrollHeight"
+        )
+
+        for i in range(3):
+            """Scroll down to the bottom"""
+            self.driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);"
+            )
+
+            """Wait to load page"""
+            time.sleep(self.holdup)
+
+            """Calculate new scroll height and compare with the last scroll height"""
+            new_scroll_height = self.driver.execute_script(
+                "return document.body.scrollHeight"
+            )
+            if new_scroll_height == last_scroll_height:
+                break
+            last_scroll_height = new_scroll_height
+
+    def scrape_profile(self, link):
+        self.driver.get(link)
+        self.scroll_profile_page()
